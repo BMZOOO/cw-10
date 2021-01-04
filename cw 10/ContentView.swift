c34d55e4533eaca682ce7983e309594a86e63052
BMZOOO <76647795+BMZOOO@users.noreply.github.com>
2021-01-04 21:30:11 +0300

    Initial Commit

---
+++ cw 10/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cw 10
+//
+//  Created by Batool  Jawaheri  on 1/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
