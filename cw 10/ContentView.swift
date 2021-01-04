55d1f79da86dd1465025761d93614f0685f2e7ea
BMZOOO <76647795+BMZOOO@users.noreply.github.com>
2021-01-04 22:18:51 +0300

    Done

--- cw 10/ContentView.swift
+++ cw 10/ContentView.swift
@@ -8,14 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    @State var Mood: String = ""
+    
+    
+    enum moodType {
+        case happy, sad , meh, fine
+        
     }
+  
+    var body: some View {
+        
+        
+        VStack{
+        Text("MoodCHeck")
+            .font(.largeTitle)
+            .bold()
+            .padding()
+        Spacer()
+        
+        Text ("I feel \(Mood)")
+            .bold()
+            .padding()
+            
+            HStack {
+                Text("😀").onTapGesture {
+                    Mood = moods (mood: .happy)
+                }
+           
+                Text("😞").onTapGesture {
+                    Mood = moods (mood: .sad)
+                }
+                Text("😕") .onTapGesture{
+                    Mood = moods (mood: .meh)
+                }
+            
+                Text("😇").onTapGesture{
+                    Mood = moods (mood: .fine)
+                }
+                
+            }
+        
+        
+        Spacer()
+        
+        }
+            
+    }
+    
+    func moods (mood: moodType) -> String {
+        if mood == .fine{
+            return "fine"
+        }
+        if mood == .sad{
+            return "sad"
+        }
+        if mood == .meh{
+            return "meh"
+        }
+        if mood == .happy{
+            return "happy"
+        }
+    return " "
+    }
+    
 }
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
